6ac5d770dd8fe416e3dbe4b73c0a3a7caa3fc254
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2024-02-26 16:25:42 +0000

    add test call

--- crates/ecci/src/main.rs
+++ crates/ecci/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let add_result = ecci_editorconfig::add(2, 2);
+    let parse_result = ecci_editorconfig::parse(std::path::Path::new("Cargo.toml"));
+    println!("Hello, world! {}", add_result);
+    println!("{}", parse_result.get("indent_style").unwrap());
 }
