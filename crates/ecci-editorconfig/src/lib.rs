e98c1d53cbb3bb366b6497647a4721302dccc3a3
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2024-02-29 12:20:23 +0000

    simplify parse

--- crates/ecci-editorconfig/src/lib.rs
+++ crates/ecci-editorconfig/src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::ffi::CString;
 use std::path::{Path, PathBuf};
 
@@ -44,80 +43,26 @@ pub struct Config {
 
 impl Config {
     pub fn from_path(path: &Path) -> std::io::Result<Config> {
-        let name_value_map = parse_internal(path)?;
-        let mut config = Config {
-            path: path.to_path_buf(),
-            indent_style: None,
-            indent_size: None,
-            indent_size_is_tab: false,
-            tab_width: None,
-            end_of_line: None,
-            charset: None,
-            trim_trailing_whitespace: None,
-            insert_final_newline: None,
-            max_line_length: None,
-        };
-        if name_value_map.contains_key("indent_style") {
-            let value = name_value_map.get("indent_style").unwrap();
-            match value.as_str() {
-                "tab" => config.indent_style = Some(IndentStyle::Tab),
-                "space" => config.indent_style = Some(IndentStyle::Space),
-                _ => {}
-            }
-        }
-        if name_value_map.contains_key("indent_size") {
-            let value = name_value_map.get("indent_size").unwrap();
-            if value == "tab" {
-                config.indent_size_is_tab = true;
-            } else {
-                config.indent_size = Some(value.parse().unwrap());
-            }
-        }
-        if name_value_map.contains_key("tab_width") {
-            let value = name_value_map.get("tab_width").unwrap();
-            config.tab_width = Some(value.parse().unwrap());
-        }
-        if name_value_map.contains_key("end_of_line") {
-            let value = name_value_map.get("end_of_line").unwrap();
-            match value.as_str() {
-                "lf" => config.end_of_line = Some(EndOfLine::LF),
-                "crlf" => config.end_of_line = Some(EndOfLine::CRLF),
-                "cr" => config.end_of_line = Some(EndOfLine::CR),
-                _ => {}
-            }
-        }
-        if name_value_map.contains_key("charset") {
-            let value = name_value_map.get("charset").unwrap();
-            match value.as_str() {
-                "latin1" => config.charset = Some(Charset::Latin1),
-                "utf-8" => config.charset = Some(Charset::UTF8),
-                "utf-16be" => config.charset = Some(Charset::UTF16BE),
-                "utf-16le" => config.charset = Some(Charset::UTF16LE),
-                "utf-8-bom" => config.charset = Some(Charset::UTF8BOM),
-                _ => {}
-            }
-        }
-        if name_value_map.contains_key("trim_trailing_whitespace") {
-            let value = name_value_map.get("trim_trailing_whitespace").unwrap();
-            config.trim_trailing_whitespace = Some(value.parse().unwrap());
-        }
-        if name_value_map.contains_key("insert_final_newline") {
-            let value = name_value_map.get("insert_final_newline").unwrap();
-            config.insert_final_newline = Some(value.parse().unwrap());
-        }
-        if name_value_map.contains_key("max_line_length") {
-            let value = name_value_map.get("max_line_length").unwrap();
-            config.max_line_length = Some(value.parse().unwrap());
-        }
-        Ok(config)
+        parse_internal(path)
     }
 }
 
-fn parse_internal(path: &Path) -> std::io::Result<HashMap<String, String>> {
+fn parse_internal(path: &Path) -> std::io::Result<Config> {
     let canonical = path.canonicalize()?;
     let c_string = CString::new(canonical.to_str().unwrap()).unwrap();
     let ptr: *const i8 = c_string.as_ptr();
-    let mut result: HashMap<String, String> = HashMap::new();
+    let mut config = Config {
+        path: path.to_path_buf(),
+        indent_style: None,
+        indent_size: None,
+        indent_size_is_tab: false,
+        tab_width: None,
+        end_of_line: None,
+        charset: None,
+        trim_trailing_whitespace: None,
+        insert_final_newline: None,
+        max_line_length: None,
+    };
     unsafe {
         let handle = bindings::editorconfig_handle_init();
         bindings::editorconfig_parse(ptr, handle);
@@ -128,14 +73,54 @@ fn parse_internal(path: &Path) -> std::io::Result<HashMap<String, String>> {
             bindings::editorconfig_handle_get_name_value(handle, i, &mut name, &mut value);
             let name = std::ffi::CStr::from_ptr(name).to_str().unwrap();
             let value = std::ffi::CStr::from_ptr(value).to_str().unwrap();
-            result.insert(name.to_string(), value.to_string());
+            match name.to_string().as_str() {
+                "indent_style" => match value {
+                    "tab" => config.indent_style = Some(IndentStyle::Tab),
+                    "space" => config.indent_style = Some(IndentStyle::Space),
+                    _ => {}
+                },
+                "indent_size" => {
+                    if value == "tab" {
+                        config.indent_size_is_tab = true;
+                    } else {
+                        config.indent_size = Some(value.parse().unwrap());
+                    }
+                }
+                "tab_width" => {
+                    config.tab_width = Some(value.parse().unwrap());
+                }
+                "end_of_line" => match value {
+                    "lf" => config.end_of_line = Some(EndOfLine::LF),
+                    "crlf" => config.end_of_line = Some(EndOfLine::CRLF),
+                    "cr" => config.end_of_line = Some(EndOfLine::CR),
+                    _ => {}
+                },
+                "charset" => match value {
+                    "latin1" => config.charset = Some(Charset::Latin1),
+                    "utf-8" => config.charset = Some(Charset::UTF8),
+                    "utf-16be" => config.charset = Some(Charset::UTF16BE),
+                    "utf-16le" => config.charset = Some(Charset::UTF16LE),
+                    "utf-8-bom" => config.charset = Some(Charset::UTF8BOM),
+                    _ => {}
+                },
+                "trim_trailing_whitespace" => {
+                    config.trim_trailing_whitespace = Some(value.parse().unwrap());
+                }
+                "insert_final_newline" => {
+                    config.insert_final_newline = Some(value.parse().unwrap());
+                }
+                "max_line_length" => {
+                    config.max_line_length = Some(value.parse().unwrap());
+                }
+                _ => {}
+            }
         }
         let ret = bindings::editorconfig_handle_destroy(handle);
         if ret != 0 {
             panic!("Failed to destroy the editorconfig_handle object");
         }
     }
-    Ok(result)
+    Ok(config)
 }
 
 #[cfg(test)]
@@ -144,14 +129,15 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = parse_internal(Path::new("testdata/test.txt")).unwrap();
-        assert_eq!(result.get("indent_style").unwrap(), "space");
-        assert_eq!(result.get("indent_size").unwrap(), "4");
-        assert_eq!(result.get("insert_final_newline").unwrap(), "true");
-
         let config = Config::from_path(Path::new("testdata/test.txt")).unwrap();
         assert_eq!(config.indent_style, Some(IndentStyle::Space));
         assert_eq!(config.indent_size, Some(4));
+        assert_eq!(config.indent_size_is_tab, false);
+        assert_eq!(config.tab_width, Some(8));
+        assert_eq!(config.end_of_line, Some(EndOfLine::LF));
+        assert_eq!(config.charset, Some(Charset::UTF8));
+        assert_eq!(config.trim_trailing_whitespace, Some(true));
         assert_eq!(config.insert_final_newline, Some(true));
+        assert_eq!(config.max_line_length, Some(100));
     }
 }
