737d14da457e185091a5896bcd047a9a5fc81ccc
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2024-02-29 12:10:03 +0000

    replace parse to from_path

--- crates/ecci-editorconfig/src/lib.rs
+++ crates/ecci-editorconfig/src/lib.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::ffi::CString;
-use std::path::Path;
+use std::fs::{self, File};
+use std::io::{BufRead, Read, Seek, SeekFrom};
+use std::path::{Path, PathBuf};
 
 #[allow(non_camel_case_types)]
 #[allow(dead_code)]
@@ -30,6 +32,7 @@ pub enum Charset {
 
 #[derive(Debug, PartialEq)]
 pub struct Config {
+    pub path: PathBuf,
     pub indent_style: Option<IndentStyle>,
     pub indent_size: Option<usize>,
     pub indent_size_is_tab: bool,
@@ -41,76 +44,79 @@ pub struct Config {
     pub max_line_length: Option<usize>,
 }
 
-pub fn parse(path: &Path) -> Config {
-    let name_value_map = parse_internal(path);
-    let mut config = Config {
-        indent_style: None,
-        indent_size: None,
-        indent_size_is_tab: false,
-        tab_width: None,
-        end_of_line: None,
-        charset: None,
-        trim_trailing_whitespace: None,
-        insert_final_newline: None,
-        max_line_length: None,
-    };
-    if name_value_map.contains_key("indent_style") {
-        let value = name_value_map.get("indent_style").unwrap();
-        match value.as_str() {
-            "tab" => config.indent_style = Some(IndentStyle::Tab),
-            "space" => config.indent_style = Some(IndentStyle::Space),
-            _ => {}
+impl Config {
+    pub fn from_path(path: &Path) -> std::io::Result<Config> {
+        let name_value_map = parse_internal(path)?;
+        let mut config = Config {
+            path: path.to_path_buf(),
+            indent_style: None,
+            indent_size: None,
+            indent_size_is_tab: false,
+            tab_width: None,
+            end_of_line: None,
+            charset: None,
+            trim_trailing_whitespace: None,
+            insert_final_newline: None,
+            max_line_length: None,
+        };
+        if name_value_map.contains_key("indent_style") {
+            let value = name_value_map.get("indent_style").unwrap();
+            match value.as_str() {
+                "tab" => config.indent_style = Some(IndentStyle::Tab),
+                "space" => config.indent_style = Some(IndentStyle::Space),
+                _ => {}
+            }
         }
-    }
-    if name_value_map.contains_key("indent_size") {
-        let value = name_value_map.get("indent_size").unwrap();
-        if value == "tab" {
-            config.indent_size_is_tab = true;
-        } else {
-            config.indent_size = Some(value.parse().unwrap());
+        if name_value_map.contains_key("indent_size") {
+            let value = name_value_map.get("indent_size").unwrap();
+            if value == "tab" {
+                config.indent_size_is_tab = true;
+            } else {
+                config.indent_size = Some(value.parse().unwrap());
+            }
         }
-    }
-    if name_value_map.contains_key("tab_width") {
-        let value = name_value_map.get("tab_width").unwrap();
-        config.tab_width = Some(value.parse().unwrap());
-    }
-    if name_value_map.contains_key("end_of_line") {
-        let value = name_value_map.get("end_of_line").unwrap();
-        match value.as_str() {
-            "lf" => config.end_of_line = Some(EndOfLine::LF),
-            "crlf" => config.end_of_line = Some(EndOfLine::CRLF),
-            "cr" => config.end_of_line = Some(EndOfLine::CR),
-            _ => {}
+        if name_value_map.contains_key("tab_width") {
+            let value = name_value_map.get("tab_width").unwrap();
+            config.tab_width = Some(value.parse().unwrap());
         }
-    }
-    if name_value_map.contains_key("charset") {
-        let value = name_value_map.get("charset").unwrap();
-        match value.as_str() {
-            "latin1" => config.charset = Some(Charset::Latin1),
-            "utf-8" => config.charset = Some(Charset::UTF8),
-            "utf-16be" => config.charset = Some(Charset::UTF16BE),
-            "utf-16le" => config.charset = Some(Charset::UTF16LE),
-            "utf-8-bom" => config.charset = Some(Charset::UTF8BOM),
-            _ => {}
+        if name_value_map.contains_key("end_of_line") {
+            let value = name_value_map.get("end_of_line").unwrap();
+            match value.as_str() {
+                "lf" => config.end_of_line = Some(EndOfLine::LF),
+                "crlf" => config.end_of_line = Some(EndOfLine::CRLF),
+                "cr" => config.end_of_line = Some(EndOfLine::CR),
+                _ => {}
+            }
         }
+        if name_value_map.contains_key("charset") {
+            let value = name_value_map.get("charset").unwrap();
+            match value.as_str() {
+                "latin1" => config.charset = Some(Charset::Latin1),
+                "utf-8" => config.charset = Some(Charset::UTF8),
+                "utf-16be" => config.charset = Some(Charset::UTF16BE),
+                "utf-16le" => config.charset = Some(Charset::UTF16LE),
+                "utf-8-bom" => config.charset = Some(Charset::UTF8BOM),
+                _ => {}
+            }
+        }
+        if name_value_map.contains_key("trim_trailing_whitespace") {
+            let value = name_value_map.get("trim_trailing_whitespace").unwrap();
+            config.trim_trailing_whitespace = Some(value.parse().unwrap());
+        }
+        if name_value_map.contains_key("insert_final_newline") {
+            let value = name_value_map.get("insert_final_newline").unwrap();
+            config.insert_final_newline = Some(value.parse().unwrap());
+        }
+        if name_value_map.contains_key("max_line_length") {
+            let value = name_value_map.get("max_line_length").unwrap();
+            config.max_line_length = Some(value.parse().unwrap());
+        }
+        Ok(config)
     }
-    if name_value_map.contains_key("trim_trailing_whitespace") {
-        let value = name_value_map.get("trim_trailing_whitespace").unwrap();
-        config.trim_trailing_whitespace = Some(value.parse().unwrap());
-    }
-    if name_value_map.contains_key("insert_final_newline") {
-        let value = name_value_map.get("insert_final_newline").unwrap();
-        config.insert_final_newline = Some(value.parse().unwrap());
-    }
-    if name_value_map.contains_key("max_line_length") {
-        let value = name_value_map.get("max_line_length").unwrap();
-        config.max_line_length = Some(value.parse().unwrap());
-    }
-    config
 }
 
-fn parse_internal(path: &Path) -> HashMap<String, String> {
-    let canonical = path.canonicalize().unwrap();
+fn parse_internal(path: &Path) -> std::io::Result<HashMap<String, String>> {
+    let canonical = path.canonicalize()?;
     let c_string = CString::new(canonical.to_str().unwrap()).unwrap();
     let ptr: *const i8 = c_string.as_ptr();
     let mut result: HashMap<String, String> = HashMap::new();
@@ -131,7 +137,7 @@ fn parse_internal(path: &Path) -> HashMap<String, String> {
             panic!("Failed to destroy the editorconfig_handle object");
         }
     }
-    result
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -140,14 +146,14 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = parse_internal(Path::new("testdata/test.txt"));
+        let result = parse_internal(Path::new("testdata/test.txt")).unwrap();
         assert_eq!(result.get("indent_style").unwrap(), "space");
         assert_eq!(result.get("indent_size").unwrap(), "4");
         assert_eq!(result.get("insert_final_newline").unwrap(), "true");
 
-        let config = parse(Path::new("testdata/test.txt"));
-        assert_eq!(config.indent_style.unwrap(), IndentStyle::Space);
-        assert_eq!(config.indent_size.unwrap(), 4);
-        assert_eq!(config.insert_final_newline.unwrap(), true);
+        let config = Config::from_path(Path::new("testdata/test.txt")).unwrap();
+        assert_eq!(config.indent_style, Some(IndentStyle::Space));
+        assert_eq!(config.indent_size, Some(4));
+        assert_eq!(config.insert_final_newline, Some(true));
     }
 }
--- crates/ecci/src/main.rs
+++ crates/ecci/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let path = std::path::Path::new("Cargo.toml");
-    let parsed_config = ecci_editorconfig::parse(path);
+    let parsed_config = ecci_editorconfig::Config::from_path(path).unwrap();
     println!(
-        "{}: indent_style: {:?}",
+        "{} indent_style:{:?}",
         path.display(),
         parsed_config.indent_style.unwrap()
     );
