1f3cdb89602eae14117297d423f6a1e0b70bed6c
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2024-03-03 05:52:02 +0000

    implement check_trim_trailing_whitespace

--- crates/ecci-checker/src/trim_trailing_whitespace.rs
+++ crates/ecci-checker/src/trim_trailing_whitespace.rs
@@ -8,5 +8,52 @@ pub fn check_trim_trailing_whitespace<T: Output>(
     line_number: usize,
     content: &str,
 ) {
-    // todo!();
+    if let Some(true) = config.trim_trailing_whitespace {
+        let trimmed = content.trim_end_matches(['\r', '\n']);
+        if trimmed.ends_with(' ') {
+            output.output(
+                line_number,
+                trimmed.len() - 1,
+                1,
+                &config.path.to_string_lossy(),
+                content,
+                "trim_trailing_whitespace",
+            )
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{check_all, MockOutput};
+
+    #[test]
+    fn check_trim_trailing_whitespace_no_error() {
+        let target_path = "../../testdata/trim_trailing_whitespace/no_error.target";
+        let config =
+            ecci_editorconfig::Config::from_path(std::path::Path::new(target_path)).unwrap();
+        let mut mock = MockOutput::new();
+        mock.expect_output().never();
+        check_all(&config, &mut mock).unwrap();
+    }
+
+    #[test]
+    fn check_trim_trailing_whitespace_error() {
+        let target_path = "../../testdata/trim_trailing_whitespace/error.target";
+        let config =
+            ecci_editorconfig::Config::from_path(std::path::Path::new(target_path)).unwrap();
+        let mut mock = MockOutput::new();
+        mock.expect_output()
+            .withf(move |line_number, column, length, path, content, rule| {
+                *line_number == 2
+                    && *column == 2
+                    && *length == 1
+                    && path == target_path
+                    && content == "b  \n"
+                    && rule == "trim_trailing_whitespace"
+            })
+            .times(1)
+            .return_const(());
+        check_all(&config, &mut mock).unwrap();
+    }
 }
