60cb8c35cf6e5742359d26d5f2de86174a67feb9
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2024-03-03 06:57:09 +0000

    implement check_insert_final_newline

--- crates/ecci-checker/src/lib.rs
+++ crates/ecci-checker/src/lib.rs
@@ -29,13 +29,20 @@ fn check_line<T: Output>(
     output: &mut T,
     line_number: usize,
     content: &str,
+    has_next_line: bool,
 ) {
     indent_style::check_indent_style(config, output, line_number, content);
     indent_size::check_indent_size(config, output, line_number, content);
     end_of_line::check_end_of_line(config, output, line_number, content);
     charset::check_charset(config, output, line_number, content);
     trim_trailing_whitespace::check_trim_trailing_whitespace(config, output, line_number, content);
-    insert_final_newline::check_insert_final_newline(config, output, line_number, content);
+    insert_final_newline::check_insert_final_newline(
+        config,
+        output,
+        line_number,
+        content,
+        has_next_line,
+    );
     max_line_length::check_max_line_length(config, output, line_number, content);
 }
 
@@ -46,54 +53,31 @@ pub fn check_all<T: Output>(config: &Config, output: &mut T) -> std::io::Result<
 
     // real implementation
     let file = std::fs::File::open(&config.path)?;
+    let filesize = file.metadata()?.len();
+    let mut readsize = 0u64;
     let mut reader = std::io::BufReader::new(file);
     let mut line_number = 0usize;
     let mut buf = Vec::new();
 
-    if let Some(EndOfLine::LF) = &config.end_of_line {
-        while reader.read_until(b'\n', &mut buf).unwrap() > 0 {
-            line_number += 1;
-            check_line(
-                config,
-                output,
-                line_number,
-                std::str::from_utf8(&buf).unwrap(),
-            );
-            buf.clear();
-        }
-    } else if let Some(EndOfLine::CRLF) = &config.end_of_line {
-        while reader.read_until(b'\n', &mut buf).unwrap() > 0 {
-            line_number += 1;
-            check_line(
-                config,
-                output,
-                line_number,
-                std::str::from_utf8(&buf).unwrap(),
-            );
-            buf.clear();
-        }
-    } else if let Some(EndOfLine::CR) = &config.end_of_line {
-        while reader.read_until(b'\r', &mut buf).unwrap() > 0 {
-            line_number += 1;
-            check_line(
-                config,
-                output,
-                line_number,
-                std::str::from_utf8(&buf).unwrap(),
-            );
-            buf.clear();
-        }
-    } else {
-        while reader.read_until(b'\n', &mut buf).unwrap() > 0 {
-            line_number += 1;
-            check_line(
-                config,
-                output,
-                line_number,
-                std::str::from_utf8(&buf).unwrap(),
-            );
-            buf.clear();
-        }
+    let eol = match &config.end_of_line {
+        Some(EndOfLine::CR) => b'\r',
+        Some(EndOfLine::LF) => b'\n',
+        Some(EndOfLine::CRLF) => b'\n',
+        None => b'\n',
+    };
+
+    while reader.read_until(eol, &mut buf).unwrap() > 0 {
+        line_number += 1;
+        readsize += buf.len() as u64;
+        let has_next_line = readsize < filesize;
+        check_line(
+            config,
+            output,
+            line_number,
+            std::str::from_utf8(&buf).unwrap(),
+            has_next_line,
+        );
+        buf.clear();
     }
     Ok(())
 }
--- testdata/generate.sh
+++ testdata/generate.sh
@@ -9,21 +9,21 @@ cd $(dirname $0)
 pushd end_of_line
     [ -d crlf ] || mkdir -p crlf
     pushd crlf
-        echo -ne "[*.target]\nend_of_line = crlf\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nend_of_line = crlf\n" > .editorconfig
         echo -ne "a\r\nb\r\nc\r\n" > no_error.target
         echo -ne "a\nb\nc\n" > error_lf.target
         echo -ne "a\rb\rc\r" > error_cr.target
     popd
     [ -d lf ] || mkdir -p lf
     pushd lf
-        echo -ne "[*.target]\nend_of_line = lf\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nend_of_line = lf\n" > .editorconfig
         echo -ne "a\nb\nc\n" > no_error.target
         echo -ne "a\r\nb\r\nc\r\n" > error_crlf.target
         echo -ne "a\rb\rc\r" > error_cr.target
     popd
     [ -d cr ] || mkdir -p cr
     pushd cr
-        echo -ne "[*.target]\nend_of_line = cr\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nend_of_line = cr\n" > .editorconfig
         echo -ne "a\rb\rc\r" > no_error.target
         echo -ne "a\r\nb\r\nc\r\n" > error_crlf.target
         echo -ne "a\nb\nc\n" > error_lf.target
@@ -35,13 +35,13 @@ popd
 pushd indent_style
     [ -d space ] || mkdir -p space
     pushd space
-        echo -ne "[*.target]\nindent_style = space\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nindent_style = space\n" > .editorconfig
         echo -ne "a\n  b\nc\n" > no_error.target
         echo -ne "a\n\t\tb\nc\n" > error_tab.target
     popd
     [ -d tab ] || mkdir -p tab
     pushd tab
-        echo -ne "[*.target]\nindent_style = tab\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nindent_style = tab\n" > .editorconfig
         echo -ne "a\n\t\tb\nc\n" > no_error.target
         echo -ne "a\n  b\nc\n" > error_space.target
     popd
@@ -52,13 +52,13 @@ popd
 pushd indent_size
     [ -d 2 ] || mkdir -p 2
     pushd 2
-        echo -ne "[*.target]\nindent_size = 2\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nindent_style = space\nindent_size = 2\n" > .editorconfig
         echo -ne "a\n  b\nc\n" > no_error.target
         echo -ne "a\n   b\nc\n" > error_3.target
     popd
     [ -d 4 ] || mkdir -p 4
     pushd 4
-        echo -ne "[*.target]\nindent_size = 4\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nindent_style = space\nindent_size = 4\n" > .editorconfig
         echo -ne "a\n    b\nc\n" > no_error.target
         echo -ne "a\n  b\nc\n" > error_2.target
     popd
@@ -67,7 +67,7 @@ popd
 # trim_trailing_whitespace
 [ -d trim_trailing_whitespace ] || mkdir -p trim_trailing_whitespace
 pushd trim_trailing_whitespace
-    echo -ne "[*.target]\ntrim_trailing_whitespace = true\n" > .editorconfig
+    echo -ne "root = true\n[*.target]\ntrim_trailing_whitespace = true\n" > .editorconfig
     echo -ne "a\nb\nc\n" > no_error.target
     echo -ne "a\nb  \nc\n" > error.target
 popd
@@ -77,8 +77,24 @@ popd
 pushd max_line_length
     [ -d 10 ] || mkdir -p 10
     pushd 10
-        echo -ne "[*.target]\nmax_line_length = 10\n" > .editorconfig
+        echo -ne "root = true\n[*.target]\nmax_line_length = 10\n" > .editorconfig
         echo -ne "a\nbbbbbbbbbb\nc\n" > no_error.target
         echo -ne "a\nbbbbbbbbbbbb\nc\n" > error.target
+    popd
+popd
+
+# insert_final_newline
+[ -d insert_final_newline ] || mkdir -p insert_final_newline
+pushd insert_final_newline
+    [ -d true ] || mkdir -p true
+    pushd true
+        echo -ne "root = true\n[*.target]\ninsert_final_newline = true\n" > .editorconfig
+        echo -ne "a\nb\nc\n" > no_error.target
+        echo -ne "a\nb\nc" > error.target
+    popd
+    [ -d false ] || mkdir -p false
+    pushd false
+        echo -ne "root = true\n[*.target]\ninsert_final_newline = false\n" > .editorconfig
+        echo -ne "a\nb\nc" > no_error.target
     popd
 popd
